f8d750d176645a4ede441369e49fa8544244457e
Sam M <mrinalini-m@users.noreply.github.com>
2020-01-04 00:34:11 -0800

    Add links and prfile pic (#3)

---
+++ src/components/Links.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="links">
+		<div class="link-wrapper"><a href="">Github</a></div>
+		<div class="link-wrapper"><a href="">Linked In </a></div>
+	</div>
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style lang="scss" scoped>
+	.links {
+		flex: 50%;
+		.link-wrapper {
+			margin: 0.5rem 1rem;
+		}
+	}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,32 @@
 <template>
 	<Layout :isIndex="true">
+		<div class="wrapper">
+			<g-image class="profile-pic" src="~/assets/images/profile.jpeg"></g-image>
+			<Links />
+		</div>
 		<Map />
 	</Layout>
 </template>
 
 <script>
 	import Map from '~/components/Map'
+	import Links from '~/components/Links'
 	export default {
 		metaInfo: {
 			title: 'Mrinalini'
 		},
 		components: {
-			Map
+			Map,
+			Links
 		}
 	}
 </script>
+
+<style>
+	.wrapper {
+		display: flex;
+	}
+	.profile-pic {
+		width: 350px;
+	}
+</style>

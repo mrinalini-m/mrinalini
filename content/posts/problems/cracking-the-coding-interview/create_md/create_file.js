2f59bc9092adb348f11b10114f2f2ec1dae12f7c
mrinalini-m <mrinalini.manandhar@gmail.com>
2020-05-28 17:48:12 -0700

    Fix slug typo: cracking the coding interview problem

--- content/posts/problems/cracking-the-coding-interview/create_md/create_file.js
+++ content/posts/problems/cracking-the-coding-interview/create_md/create_file.js
@@ -1,6 +1,6 @@
 const exec = require('child_process').exec
 
-const getName = (str) => {
+const getName = str => {
 	let arr = str.split('.')
 
 	arr.pop()
@@ -14,23 +14,27 @@ const getName = (str) => {
 	return split
 }
 
-const convertToMd = (js) => {
+const convertToMd = js => {
 	return js.replace('.js', '.md')
 }
 const name = getName(process.argv[2]),
-	ch = `Ch0${name.shift()}`,
+	ch = `${name.shift()}`,
 	no = name.shift(),
 	lowerCase = []
 
 for (const word of name) {
 	lowerCase.push(word.toLowerCase())
 }
-const title = `${ch}-${no} - ${name.join(' ')} [Cracking the Coding Interview]`,
+const title = `${no} - ${name.join(' ')} [Cracking the Coding Interview]`,
 	date = '2020-04-20',
 	tags = JSON.stringify(['"tree"']),
-	slug = `ctci-ch0${ch}-${no}-${lowerCase.join('-')}`,
+	slug = `ctci-${ch}-${no}-${lowerCase.join('-')}`,
 	fileName = convertToMd(process.argv[2])
 
 exec(
-	`touch ${fileName} && code ${fileName} && node ./../create_md/file_content.js "${title}" "${date}" "${slug}" "${tags}" > ${fileName} && cat ${process.argv[2]} >> ${fileName} && node ./../create_md/end.js >> ${fileName} && rm ${process.argv[2]}`
+	`touch ${fileName} && code ${fileName} && node ./../create_md/file_content.js "${title}" "${date}" "${slug}" "${tags}" > ${fileName} && cat ${
+		process.argv[2]
+	} >> ${fileName} && node ./../create_md/end.js >> ${fileName} && rm ${
+		process.argv[2]
+	}`
 )
